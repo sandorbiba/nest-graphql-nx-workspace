9f9128e2e9bd5d613693b1582f0136e419a97676
Sandor Biba <sandorbiba@gmail.com>
2021-05-06 21:24:32 +0200

    add GraphQL

--- libs/core/src/lib/core.module.ts
+++ libs/core/src/lib/core.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { configuration } from './config/configuration'
 import { validationSchema } from './config/validation'
+import { GraphQLModule } from '@nestjs/graphql'
+import { CoreResolver } from './core.resolver'
 
 @Module({
   imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
@@ -12,7 +17,7 @@ import { validationSchema } from './config/validation'
     }),
   ],
   controllers: [],
-  providers: [],
+  providers: [CoreResolver],
   exports: [],
 })
 export class CoreModule {}
